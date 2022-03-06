ca077871cf6265d74319b9bad1567f695ad170c4
Julian Bull <julianbullmagic@gmail.com>
2022-03-07 09:13:20 +1100

    fff

--- server.js
+++ server.js
@@ -55,10 +55,10 @@ const PORT = process.env.PORT || 5000
 const app = express();
 const server = require("http").createServer(app);
 
-
+let origin=process.env.PORT||"http://localhost:5000"||"http://localhost:3000"
 const io = require("socket.io")(server, {
   cors: {
-    origin: process.env.PORT||"http://localhost:5000"||"http://localhost:3000"||"https://democratic-social-network.herokuapp.com",
+    origin: "https://democratic-social-network.herokuapp.com",
     methods: ["GET", "POST"]
   }
 });
