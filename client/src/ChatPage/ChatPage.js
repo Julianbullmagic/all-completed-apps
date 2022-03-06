f64a10c9d9799b45de942d3466cabb94a51cdd70
Julian Bull <julianbullmagic@gmail.com>
2022-03-07 09:38:18 +1100

    fff

--- client/src/ChatPage/ChatPage.js
+++ client/src/ChatPage/ChatPage.js
@@ -56,13 +56,12 @@ componentWillReceiveProps(nextProps) {
 componentDidMount(props) {
     let server ="http://localhost:5000";
     this.props.dispatch(getChats());
-    // if(process.env.NODE_ENV=="production"){
-    //   this.socket=io();
-    // }
-    // if(process.env.NODE_ENV=="development"){
-    //   this.socket=io(server);
-    // }
-    this.socket=io();
+    if(process.env.NODE_ENV=="production"){
+      this.socket=io();
+    }
+    if(process.env.NODE_ENV=="development"){
+      this.socket=io(server);
+    }
 
     this.setInitialChats()
 
