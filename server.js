463c512f070fd26ca46a4c52c80b7478370d4b34
Julian Bull <julianbullmagic@gmail.com>
2022-03-11 18:43:01 +1100

    ffff

--- server.js
+++ server.js
@@ -47,14 +47,10 @@ cloudinary.config({
   secure: true
 });
 
-
-
 //comment out before building for production
 const PORT = process.env.PORT||5000
 
-
 const app = express();
-
 const server = require("http").createServer(app);
 let origin
 
