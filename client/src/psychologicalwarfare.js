77fe61a3a55c7b397b2b7f4bae990a73329b278c
Julian Bull <julianbullmagic@gmail.com>
2022-08-03 10:04:00 +1000

    ffff

--- client/src/psychologicalwarfare.js
+++ client/src/psychologicalwarfare.js
@@ -94,5 +94,11 @@ Note on Pasternak's Novel, Dr Zhivago</p>
 <p>36) <a href="https://www.cia.gov/readingroom/docs/CIA-RDP91-00901R000600300001-1.pdf">https://www.cia.gov/readingroom/docs/CIA-RDP91-00901R000600300001-1.pdf</a>
 Opinion Commentary A Short Course In The Secret War page 1</p>
 <p>37) Dov H. Levin 2019.   “Partisan Electoral Interventions by the Great Powers: Introducing the PEIG Dataset” Conflict Management and Peace Science, 36 (1): 88-106</p>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/tTBWfkE7BXU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/-zIqCH00V4I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/EuwmWnphqII" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/34LGPIXvU5M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/xL3b_QmNV2Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/xgKPyIj8Q60" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   )}
