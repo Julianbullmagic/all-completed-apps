c6929d4f1154a4bc90c7654d06ed5320f02b8a55
Julian Bull <julianbullmagic@gmail.com>
2022-09-07 15:53:13 +1000

    ffff

--- client/src/ChatPage/Sections/ChatCard.js
+++ client/src/ChatPage/Sections/ChatCard.js
@@ -4,7 +4,7 @@ import moment from 'moment';
 function ChatCard(props) {
     return (
         <div style={{ width:'95%',background:"#e0e8e6",margin:"1vw",padding:"10px",overflow:"auto" }}>
-        <p style={{margin:"0.5vw"}}><strong>{props.sender.name}:</strong> {props.message}</p>
+        <p style={{margin:"0.5vw"}}><strong>{props.sender&&<>{props.sender.name?<>{props.sender.name}</>:<></>}</>}</strong> {props.message}</p>
         </div>
     )
 }
