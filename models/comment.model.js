12ae7abcc2ae4c924fd9c1ccb3e5f8fd1a7f4035
Julian Bull <julianbullmagic@gmail.com>
2022-11-05 11:30:41 +1100

    ffff

--- models/comment.model.js
+++ models/comment.model.js
@@ -1,7 +1,7 @@
 const mongoose =require( 'mongoose')
 const commentSchema = new mongoose.Schema({
   comment: {type: String},
-  postid:{type: mongoose.Schema.ObjectId, ref: 'Post'},
+  postid:{type: String},
   createdby: {type: mongoose.Schema.ObjectId, ref: 'User'},
   timecreated: Number
 })
