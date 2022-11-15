346a2a361303b670f8ef7b853599bdf386a2ad30
Julian Bull <julianbullmagic@gmail.com>
2022-11-15 22:03:52 +1100

    ffff

--- client/src/Info.js
+++ client/src/Info.js
@@ -3,6 +3,8 @@ import ip from 'ip-in';
 
 export default function Info() {
   const [ready, setReady] = useState(false)
+  const [extraReady, setExtraReady] = useState(false)
+
   const [user, setUser] = useState("")
 
   useEffect(()=> {
@@ -52,6 +54,9 @@ export default function Info() {
     if(pagecounter.psychologicalwar.includes(visitorinfo)&&pagecounter.info.includes(visitorinfo)){
       setReady(true)
       }
+      if(pagecounter.info.includes(visitorinfo)||pagecounter.tennomar.includes(visitorinfo)||pagecounter.neatugua.includes(visitorinfo)){
+        setExtraReady(true)
+        }
     }
 
 return (
@@ -78,7 +83,7 @@ return (
   government. This democracy has huge benefits for their economy, a very generous social security system, the cost of living is relatively cheap
   and wage growth is fairly rapid.
   </p>
-  <h4>Read More</h4>
+  <h2>Read More</h2>
   <img style={{width:"20vw"}} src={require('./tenomarflag.png')} />
   </div>
   </a>
@@ -88,26 +93,8 @@ return (
   <p>
   Thanks to the release of numerous formerly classified CIA documents, we now know the truth about Neatugua. The leaders of Neatugua are radical humanists who have dramatically improved the quality of life of the the Neatuguan people. The government of Neatugua has taken large steps towards guaranteeing that all Neatuguans will have their basic needs met, massively reducing poverty and guaranteeing the necessities of life are affordable and in many cases provided for free at public expense. Housing, public transport, electricity, university, childcare and many other essential services are either free or cheap. The Humanist party have greatly improved that status of African people and women, eliminating the worst aspects of racism and sexism. The standards of health and education are the best in the third world and in many ways as good or better than some first world countries. The Party created a much more fair and equitable distribution of wealth than Capitalist countries. Their private sector is largely worker-cooperatives, 30% of their GDP is produced by democratic businesses and self-employed people where the workers are the shareholders. Their public sector is somewhat like an enormous co-operative in which all Neatuguans are a member and the goal is to meet human needs instead of generate profits. Their society is highly unionised with the vast majority of their people in at least one union and most work arrangements negotiated through unions. Recently, the sustainable development index found Cuba to be the most sustainable country in the world.
   </p>
-  <h4>Read More</h4>
+  <h2>Read More</h2>
   <img style={{width:"20vw"}} src={require('./neatuguaflag.png')} />
-  </div>
-  </a>
-  <a style={{color:"black",textDecoration:"none",display:"block"}} href="/ManufacturingConsentVideos">
-  <div className="articlelink">
-  <h2>Manufacturing Consent, the mass media is much less democratic than it seems</h2>
-  <p>
-  <center><iframe className="infovid" seamless frameborder="0" scrolling="no" src="https://www.youtube.com/embed/34LGPIXvU5M"></iframe></center>
-  </p>
-  <h4>See More Videos</h4>
-  </div>
-  </a>
-  <a style={{color:"black",textDecoration:"none",display:"block"}} href="/cooperativesvideos">
-  <div className="articlelink">
-  <h2>Cooperatives Videos</h2>
-  <p>
-  <center><iframe className="infovid" seamless frameborder="0" scrolling="no" src="https://www.youtube.com/embed/8ZoI0C1mPek"></iframe></center>
-  </p>
-  <h4>See More Videos</h4>
   </div>
   </a>
   <a style={{color:"black",textDecoration:"none",display:"block"}} href="/Cooperatives">
@@ -120,16 +107,35 @@ return (
   consult and discuss with them in order to try to reach decisions that most people are happy with. This form of organization has many benefits,
   co-ops are usually more productive, have a longer lifespan, are less risky to startup and workers are better paid and taken care of.
   </p>
-  <h4>Read More</h4>
+  <h2>Read More</h2>
   </div>
   </a>
+  {extraReady&&<>
+    <a style={{color:"black",textDecoration:"none",display:"block"}} href="/ManufacturingConsentVideos">
+    <div className="articlelink">
+    <h2>Manufacturing Consent, the mass media is much less democratic than it seems</h2>
+    <p>
+    <center><iframe className="infovid" seamless frameborder="0" scrolling="no" src="https://www.youtube.com/embed/34LGPIXvU5M"></iframe></center>
+    </p>
+    <h2>See More Videos</h2>
+    </div>
+    </a>
+    <a style={{color:"black",textDecoration:"none",display:"block"}} href="/cooperativesvideos">
+    <div className="articlelink">
+    <h2>Cooperatives Videos</h2>
+    <p>
+    <center><iframe className="infovid" seamless frameborder="0" scrolling="no" src="https://www.youtube.com/embed/8ZoI0C1mPek"></iframe></center>
+    </p>
+    <h2>See More Videos</h2>
+    </div>
+    </a>
   <a style={{color:"black",textDecoration:"none",display:"block"}} href="/libertariansocialismvideos">
   <div className="articlelink">
   <h2>Libertarianism</h2>
   <p>
   </p>
   <center><iframe className="infovid" seamless frameborder="0" scrolling="no" src="https://www.youtube.com/embed/pTzC_QqSqwc"></iframe></center>
-  <h4>See More Videos</h4>
+  <h2>See More Videos</h2>
   </div>
   </a>
   <a style={{color:"black",textDecoration:"none",display:"block"}} href="/Paris1871">
@@ -141,7 +147,7 @@ return (
   economic policies, the eradication of homelessness, hunger and prostitution. Numerous feministic policies were created, such as equal pay for
   equal work and the abolishion of rules that discriminated against single or widowed mothers.
   </p>
-  <h4>Read More</h4>
+  <h2>Read More</h2>
   </div>
   </a>
   <a style={{color:"black",textDecoration:"none",display:"block"}} href="/democracyvideos">
@@ -150,7 +156,7 @@ return (
   <p>
   </p>
   <center><iframe className="infovid" seamless frameborder="0" scrolling="no" src="https://www.youtube.com/embed/NpiqX6ouM-8"></iframe></center>
-  <h4>See More Videos</h4>
+  <h2>See More Videos</h2>
   </div>
   </a>
   <a style={{color:"black",textDecoration:"none",display:"block"}} href="/Democracy">
@@ -161,7 +167,7 @@ return (
   rig the system in their own favour. For the last few decades this problem has grown gradually worse and worse and is the fundamental reason we are
   dealing with a cost of living crisis, declining wage growth and much higher unemployment rates.
   </p>
-  <h4>Read More</h4>
+  <h2>Read More</h2>
   </div>
   </a>
   <a style={{color:"black",textDecoration:"none",display:"block"}} href="/Psychological-War">
@@ -170,8 +176,9 @@ return (
   <p>
   Since the birth of humanism, the Capitalist ruling class have gone to great lengths to destroy it. They have tried their absolute best to destroy people's perception of Humanism, spreading slander throughout the mass media and academic institutions. They have infiltrated many real humanist organisations and created many of their own fake ones. Divide and conquer is their primary strategy, if they perceive any divisions between humanist groups, they try to drive a wedge into it. When they build their own Humanist organisations, the most common strategy is to spread ideas that sound humanistic, but are ultimately not at all useful. They will try to encourage a puritanical, vengeful, arrogant and self righteous mentality, any revolution that is less than perfect is bluntly dismissed, there is no constructive criticism or acknowledgement of the achievements of real viable humanist revolutions. They won't support any humanist experiments unless they failed, didn't last very long or didn't produce any measurable, lasting benefits to people's lives. The goal is to leave people with the feeling that humanism is a Utopian fantasy, idealistic, unrealistic, a nice idea, but not practical. The bourgeoisie have also tried to meddle in the elections of countries where the population shows strong support for humanism, attempting to get more right wing candidates elected.
   </p>
-  <h4>Read More</h4>
+  <h2>Read More</h2>
   </div>
   </a>
+  </>}
   </div>
 )}
