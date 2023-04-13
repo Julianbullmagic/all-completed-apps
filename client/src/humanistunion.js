e6f478d9a026aff2052d9c5ab1474b0526e63c23
Julian Bull <julianbullmagic@gmail.com>
2023-04-13 20:01:30 +1000

    ffff

--- client/src/humanistunion.js
+++ client/src/humanistunion.js
@@ -24,6 +24,7 @@ console.error(err);
 
   return (
     <div style={{marginLeft:"5vw",marginTop:"3vw",width:"90vw"}}>
+    <br/><br/><br/><br/>
     <h1>The Humanist Union</h1>
     <iframe src={"https://www.youtube.com/embed/7zbjWPLnbPw"} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
